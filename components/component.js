8526e2aaf75b49772ba42e57516c33345f408ada
SokChanSeiha <sokchanseiha@gmail.com>
2025-05-18 11:26:49 +0700

    adding footer component

--- components/component.js
+++ components/component.js
@@ -1,9 +1,16 @@
 import { headerHTML } from './header.js';
+import { footerHTML } from './footer.js';
 
 document.addEventListener("DOMContentLoaded", function () {
+  // Insert header
   const header = document.querySelector("header");
   if (header) {
     header.innerHTML = headerHTML;
   }
+  
+  // Insert footer
+  const footer = document.querySelector("footer");
+  if (footer) {
+    footer.innerHTML = footerHTML;
+  }
 });
-
