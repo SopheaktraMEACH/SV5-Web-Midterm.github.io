052d9d8de9580612fb6f78a44b8e75b8a3c6623b
SokChanSeiha <sokchanseiha@gmail.com>
2025-05-20 00:00:33 +0700

    header dynamic, detail, donate

--- components/component.js
+++ components/component.js
@@ -1,5 +1,5 @@
-import { headerHTML } from './header.js';
-import { footerHTML } from './footer.js';
+import { headerHTML } from "./header.js";
+import { footerHTML } from "./footer.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   // Insert header
@@ -7,10 +7,28 @@ document.addEventListener("DOMContentLoaded", function () {
   if (header) {
     header.innerHTML = headerHTML;
   }
-  
+
   // Insert footer
   const footer = document.querySelector("footer");
   if (footer) {
     footer.innerHTML = footerHTML;
   }
+
+  const currentPage = window.location.pathname.split("/").pop();
+
+  document.querySelectorAll(".navmenu a").forEach((link) => {
+    const linkHref = link.getAttribute("href").split("/").pop();
+    if (linkHref === currentPage) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+
+  document.querySelectorAll('.card.animated').forEach(card => {
+  card.addEventListener('click', () => {
+    window.location.href = './course-detail.html'; // target page URL
+  });
+});
+
 });
